d65604aac603aa3b71a4db71165286897e2288b0
tuxalket <alketcami@gmail.com>
2019-01-22 17:52:00 -0500

    Second Commit

--- src/todobackend/settings.py
+++ src/todobackend/settings.py
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'rest_framework',
+	'corsheaders',
+	'todo'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#cors_settings
+
+CORS_ORIGIN_ALLOW_ALL = True
